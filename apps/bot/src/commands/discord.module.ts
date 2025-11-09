04014ce92245b8acac65298eead2f538a2588103
Ayush <ayushedith@gmail.com>
2025-11-09 23:29:28 +0530

    Add DeploymentsModule to Discord module and export DeploymentsService

--- apps/bot/src/commands/discord.module.ts
+++ apps/bot/src/commands/discord.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DeploymentsModule } from '../deployments/deployments.module';
 import { DiscordClientProvider } from './discord.provider';
 import { DiscordBootstrapService } from './discord.service';
 import { CommandRegistry } from './registry.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, DeploymentsModule],
   providers: [DiscordClientProvider, DiscordBootstrapService, CommandRegistry],
   exports: [DiscordBootstrapService]
 })
